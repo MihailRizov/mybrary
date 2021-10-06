ce215d26e3bd71c9cc1588adee706ca5c67d604c
Mihail <mihail.rizov@conquex.com>
2021-10-06 16:04:28 +0300

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
+let Book;
+if (mongoose.models.Book) {
+    Book = mongoose.model('Book');
+} else {
+    Book = require('./book');
+}
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+    }
+});
+
+authorSchema.pre('remove', async function(next) {
+    try {
+        const books = await Book.find({ author: this.id });
+        if (books.length > 0) {
+            throw new Error('This author has books still');
+        }
+        
+        next();
+    } catch (error) {
+        next(error);
     }
 });
 
--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const mongoose = require('mongoose');
+const author = require('../models/author');
+
+let Book;
+if (mongoose.models.Book) {
+    Book = mongoose.model('Book');
+} else {
+    Book = require('../models/book');
+}
 
 // All Authors route
 router.get('/', async (req, res) => {
@@ -42,8 +51,8 @@ router.post('/', async (req, res) => {
     
     try {
         const newlyCreatedAuthor = await newAuthor.save();
-        // res.redirect(`authors/${newlyCreatedAuthor.id}`);
-        res.redirect(`authors`);
+        res.redirect(`authors/${newlyCreatedAuthor.id}`);
+        // res.redirect(`authors`);
     } catch (error) {
         res.render('authors/new', { 
             author: newAuthor,
@@ -51,5 +60,66 @@ router.post('/', async (req, res) => {
         }); // That way the form will be populated with the value of the user
     }
 });
+
+// Author page route
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id.trim());
+        const books = await Book.find({ author: author.id }).limit(6).exec();
+        res.render('authors/show', { author: author, booksByAuthor: books });
+    } catch (error) {
+        res.redirect('/');
+    }
+});
+
+// Edit page route
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id.trim());
+        res.render('authors/edit', { author: author });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/authors');
+    }
+});
+
+// Edit route
+router.put('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id.trim());
+        author.name = req.body.name;
+        await author.save();
+        res.redirect(`/authors/${author.id}`);
+        // res.redirect(`authors`);
+    } catch (error) {
+        if (author == null) {
+            res.redirect('/'); // No author was found
+        } else {
+            res.render('/authors/edit', { 
+                author: author,
+                errorMessage: 'Error updating author'
+            }); // That way the form will be populated with the value of the user
+        }
+        
+    }
+});
+
+// Delete route
+router.delete('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id.trim());
+        await author.remove();
+        res.redirect(`/authors`);
+    } catch (error) {
+        if (author == null) {
+            res.redirect('/'); // No author was found
+        } else {
+            res.redirect(`/authors/${author.id}`);// That way the form will be populated with the value of the user
+        }
+        
+    }
+});  
 
 module.exports = router;
